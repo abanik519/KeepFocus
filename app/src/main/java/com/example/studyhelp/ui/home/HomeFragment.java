dd5ae90c1fa21f1441946ae72d06c2f57fac1264
abnik519 <aritra.banik519@gmail.com>
2020-03-21 03:42:53 -0400

    Timer is functional

--- app/src/main/java/com/example/studyhelp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/studyhelp/ui/home/HomeFragment.java
@@ -1,7 +1,13 @@
 package com.example.studyhelp.ui.home;
 
+import android.app.Activity;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,17 +15,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.example.studyhelp.MainActivity;
 import com.example.studyhelp.R;
 
 import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
+    // Physical App elements
     private TextView text;
     private EditText user;
     private EditText userWork;
@@ -29,13 +40,15 @@ public class HomeFragment extends Fragment {
     private Button work;
     private Button rest;
     private ProgressBar bar;
-    private long input;
 
-    private CountDownTimer timer;
+    // Background elements
+    private long input;
     private long startTime;
+    private int counter = 0;
+    private CountDownTimer timer;
     private long timeLeft;
     private boolean running;
-    private int counter = 1;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -50,52 +63,41 @@ public class HomeFragment extends Fragment {
         rest = root.findViewById(R.id.rest);
         bar = root.findViewById(R.id.bar);
 
+        user.setTag(null);
+        user.addTextChangedListener(timeWatcher);
+
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String str = user.getText().toString();
+                if(str.length() == 0){ // Make sure user inputs something
+                    Toast.makeText(getActivity(), "Field can't be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 String[] time = str.split(":");
-                if(time.length == 3){
-                    input = Long.parseLong(time[0]) * 3600000;
-                    input += Long.parseLong(time[1]) * 60000;
-                    input += Long.parseLong(time[2]) * 1000;
+                for(String a: time) {
+                    if (a.length() > 2) { // Make sure its a valid time
+                        Toast.makeText(getActivity(), "Please enter a valid time", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
                 }
-                else if(time.length == 2){
-                    input = Long.parseLong(time[0]) * 60000;
-                    input += Long.parseLong(time[1]) * 1000;
+
+                input = getTime(time);
+
+                if(input == 0){ // Check if positive number
+                    Toast.makeText(getActivity(), "Please enter a positive number", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                else{
-                    input = Long.parseLong(time[0]) * 1000;
-                }
-                if (counter == 1){
+
+                // Only change the start time if the user actually changes it
+                if(counter == 0){
                     startTime = input;
                 }
-                counter++;
+                counter = 1;
                 timeLeft = input;
                 startStop();
             }
         });
-
-        work.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!running) {
-                    String str = userWork.getText().toString();
-                    user.setText(str);
-                }
-            }
-        });
-
-        rest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!running) {
-                    String str = userRest.getText().toString();
-                    user.setText(str);
-                }
-            }
-        });
-
 
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,9 +106,121 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        work.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!running && startTime == 0) {
+                    String str = userWork.getText().toString();
+                    if(str.length() == 0){ // Make user inputs something
+                        Toast.makeText(getActivity(), "Field can't be empty", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    user.setText(str);
+                    String[] time = str.split(":");
+                    for(String a: time) {
+                        if (a.length() > 2) {// Make sure its a valid time
+                            Toast.makeText(getActivity(), "Please enter a valid time", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                }else if(!running) {
+                    String str = userWork.getText().toString();
+                    if(str.length() == 0){ // Make user inputs something
+                        Toast.makeText(getActivity(), "Field can't be empty", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    user.setText(str);
+                    String[] time = str.split(":");
+                    for(String a: time) {
+                        if (a.length() > 2) { // Make sure its a valid time
+                            Toast.makeText(getActivity(), "Please enter a valid time", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                    startTime = getTime(time);
+                    resetTimer();
+                }
+            }
+        });
+
+        rest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!running && startTime == 0) {
+                    String str = userRest.getText().toString();
+                    if(str.length() == 0){ // Make user inputs something
+                        Toast.makeText(getActivity(), "Field can't be empty", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    user.setText(str);
+                    String[] time = str.split(":");
+                    for(String a: time) {
+                        if (a.length() > 2) { // Make sure its a valid time
+                            Toast.makeText(getActivity(), "Please enter a valid time", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                }else if(!running) {
+                    String str = userRest.getText().toString();
+                    if(str.length() == 0){ // Make user inputs something
+                        Toast.makeText(getActivity(), "Field can't be empty", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    user.setText(str);
+                    String[] time = str.split(":");
+                    for(String a: time) {
+                        if (a.length() > 2) { // Make sure its a valid time
+                            Toast.makeText(getActivity(), "Please enter a valid time", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                    startTime = getTime(time);
+                    resetTimer();
+                }
+            }
+        });
         return root;
     }
 
+    // This will check when the time is changed
+    private final TextWatcher timeWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if(user.getTag() == null){
+                // This will allows us to change the start time when the user changes the time
+                counter = 0;
+            }
+        }
+    };
+
+    // Gets the time from the users input
+    private long getTime(String[] time){
+        long output;
+        if(time.length == 3){
+            output = Long.parseLong(time[0]) * 3600000;
+            output += Long.parseLong(time[1]) * 60000;
+            output += Long.parseLong(time[2]) * 1000;
+        }
+        else if(time.length == 2){
+            output = Long.parseLong(time[0]) * 60000;
+            output += Long.parseLong(time[1]) * 1000;
+        }
+        else{
+            output = Long.parseLong(time[0]) * 1000;
+        }
+
+        return output;
+    }
     private void startStop(){
         if(running){
             stopTimer();
@@ -117,6 +231,7 @@ public class HomeFragment extends Fragment {
     }
 
 
+    // Starts the timer
     private void startTimer(){
         timer = new CountDownTimer(timeLeft, 1000) {
             @Override
@@ -129,19 +244,24 @@ public class HomeFragment extends Fragment {
             public void onFinish() {
                 running = false;
                 start.setText("START");
+                bar.setProgress(0);
+
+                // Alarm
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+                Ringtone r = RingtoneManager.getRingtone(getActivity(), notification);
+                r.play();
             }
         }.start();
-
-        start.setText("PAUSE");
-        reset.setVisibility(View.INVISIBLE);
+        start.setText("PAUSE");                     // Update view
         user.setVisibility(View.INVISIBLE);
         text.setVisibility(View.VISIBLE);
+        reset.setVisibility(View.INVISIBLE);
         running = true;
     }
 
     private void stopTimer(){
         timer.cancel();
-        start.setText("START");
+        start.setText("START");                    // Update view
         reset.setVisibility(View.VISIBLE);
         user.setVisibility(View.VISIBLE);
         text.setVisibility(View.INVISIBLE);
@@ -151,13 +271,12 @@ public class HomeFragment extends Fragment {
     private void resetTimer(){
         timeLeft = startTime;
         updateTimer();
-        text.setVisibility(View.INVISIBLE);
-        user.setVisibility(View.VISIBLE);
         reset.setVisibility(View.INVISIBLE);
     }
 
+
     private void updateTimer(){
-        int hours = (int) (timeLeft / 1000) / 3600;
+        int hours = (int) (timeLeft / 1000) / 3600;             // Convert from milliseconds
         int minutes = (int) ((timeLeft / 1000) % 3600) / 60;
         int seconds = (int) (timeLeft / 1000) % 60;
 
@@ -167,16 +286,11 @@ public class HomeFragment extends Fragment {
         }else {
             timeLeftText = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         }
+        user.setTag("SIKE");        // Throw of the timeWatcher
         user.setText(timeLeftText);
+        user.setTag(null);
         text.setText(timeLeftText);
-        counter++;
-        int status;
-        if(counter%2 ==0){
-            status = 100;
-        }
-        else{
-            status = 0;
-        }
+        int status = (100*(int)timeLeft)/((int)startTime); // Make the status bar tik
         bar.setProgress(status);
     }
 }
